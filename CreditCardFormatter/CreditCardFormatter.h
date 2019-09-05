b119f17b1c5b35f25df805705c8eab91b55599c8
Johan Pérez <jpeeri@gmail.com>
2019-09-05 13:58:28 +0200

    Initial Commit

---
+++ CreditCardFormatter/CreditCardFormatter.h
@@ -0,0 +1,19 @@
+//
+//  CreditCardFormatter.h
+//  CreditCardFormatter
+//
+//  Created by barbarity on 05/09/2019.
+//  Copyright © 2019 Barbarity Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CreditCardFormatter.
+FOUNDATION_EXPORT double CreditCardFormatterVersionNumber;
+
+//! Project version string for CreditCardFormatter.
+FOUNDATION_EXPORT const unsigned char CreditCardFormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreditCardFormatter/PublicHeader.h>
+
+
